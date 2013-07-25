584a06610a00047155800da66b65a22a03e52299
Ricky Zein <rrzein@gmail.com>
2013-07-25 14:13:43 -0700

    Security update

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+unless Rails.env.production?
+  config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+  config.merge! config.fetch(Rails.env, {})
+  config.each do |key, value|
+      ENV[key] = value.to_s unless value.kind_of? Hash
+  end
+end
+
 # Initialize the rails application
 Versa::Application.initialize!
