3c389a43d051e007afea50a738e32b28d81c7fbe
Ricky Zein <rrzein@gmail.com>
2013-07-25 14:08:16 -0700

    Updated security

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,9 @@
+config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+config.merge! config.fetch(Rails.env, {})
+config.each do |key, value|
+    ENV[key] = value.to_s unless value.kind_of? Hash
+end
+
 Versa::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
